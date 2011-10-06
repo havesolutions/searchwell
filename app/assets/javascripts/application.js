1a7d1a801d24ade306d55ca715019cf0f4211d54
Kalpesh <kalpesh.fulpagare@gmail.com>
2011-10-06 20:17:11 +0530

    Removed loading of ActiveAdmin Assets from Home Page, Added Email Validation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,7 +6,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
+//require_tree .
 $(document).ready(function(){
   $("#query_question").watermark("Enter your query here");
   $("#query_email").watermark("john@example.com");
--- app/models/query.rb
+++ app/models/query.rb
@@ -1,6 +1,10 @@
 class Query < ActiveRecord::Base
   after_create :send_notification_mail
-  validates :email, :question, :presence => true
+  validates :question, :presence => true
+  #validates_format_of :email, :with => /^(?=.*\d)(?=.*([a-z]|[A-Z]))([\x20-\x7E]){8,40}$/
+  validates :email,
+            :presence => true,
+            :format => { :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i }
 
   def send_notification_mail
     Mailer.delay(:run_at => (Time.now + 5.seconds)).notification_email(self)
